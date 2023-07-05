411d2a20c5314a21150eaf3dc280e72c902b3de0
Jacob Kidd <kiddj1@students.westerntc.edu>
2023-07-05 15:01:27 -0500

    Assignment 2 started

---
+++ console_apps/WebsiteBlogs/Models/User.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace WebsiteBlogs.Models;
+
+public class User{
+    public int Id{get; set;}
+    public string FirstName{get; set;}
+    public string LastName{get; set;}
+    public string Email{get; set;}
+    public int? BlogId{get; set;}
+    [ForeignKey("BlogTable")]
+    
+    public virtual List<Comment>? Comments{get; set;}
+}
