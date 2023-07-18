9e4733a902e42be2ae9fb42a7a8ba1d75670d1d7
Jacobwkidd <kiddj1@students.westerntc.edu>
2023-07-17 22:56:51 -0600

    homework

--- console_apps/WebsiteBlogs/Interfaces/IDatabaseService.cs
+++ console_apps/WebsiteBlogs/Interfaces/IDatabaseService.cs
@@ -7,41 +7,41 @@ namespace WebsiteBlogs.Interfaces;
 public interface IDatabaseService
 {
     // This method should return a list of all users in the database, including their blogs, posts, and comments.
-    Task<List<User>> GetAllUsers();
-
+    Task<List<User>> GetAllUsers();  //done
+    
     // This method should return a list of all blogs in the database, including their associated users, posts, and comments.
-    Task<List<Blog>> GetAllBlogs();
+    Task<List<Blog>> GetAllBlogs();   //done
 
     // This method should return a list of all posts in the database, including their associated blog, user, comments, and tags.
-    Task<List<Post>> GetAllPosts();
+    Task<List<Post>> GetAllPosts(); //done
 
     // This method should return a user with the specified id, including their blogs, posts, and comments.
-    Task<User?> GetUserById(int id);
+    Task<User?> GetUserById(int id); //done
 
     // This method should create a new user in the database.
-    Task AddUser(User user);
+    Task AddUser(User user); // done
 
     // This method should update an existing user in the database.
-    Task UpdateUser(User user);
+    Task UpdateUser(User user); //done
 
     // This method should delete a user with the specified id from the database.
-    Task DeleteUser(int id);
+    Task DeleteUser(int id); // done
 
     // This method should create a new blog in the database.
-    Task AddBlog(Blog blog);
+    Task AddBlog(Blog blog); // done
 
     // This method should update an existing blog in the database.
-    Task UpdateBlog(Blog blog);
+    Task UpdateBlog(Blog blog); // done
 
     // This method should delete a blog with the specified id from the database.
-    Task DeleteBlog(int id);
+    Task DeleteBlog(int id); // done
 
     // This method should create a new post in the database.
-    Task AddPost(Post post);
+    Task AddPost(Post post); // done
 
     // This method should update an existing post in the database.
-    Task UpdatePost(Post post);
+    Task UpdatePost(Post post); // done
 
     // This method should delete a post with the specified id from the database.
-    Task DeletePost(int id);
+    Task DeletePost(int id); // done
 }
--- console_apps/WebsiteBlogs/Services/DatabaseServices.cs
+++ console_apps/WebsiteBlogs/Services/DatabaseServices.cs
@@ -43,4 +43,52 @@ public class DatabaseService : IDatabaseService
                                        
             await _context.SaveChangesAsync();
         }
+        public async Task UpdateUser(User user){
+            var singleUser = await _context.User.FindAsync(user);
+            singleUser.FirstName = newFirstName;
+            singleUser.LastName = newLastName;
+            await _context.SaveChangesAsync();
+        }
+        public async Task DeleteUser(int id){
+            var user = await _context.User
+                                          .SingleOrDefaultAsync(singleUser => singleUser.id == id);
+            if(user != null){
+                _context.User.Remove(user);
+                await _context.SaveChangesAsync();
+            }
+        }
+        public async Task AddBlog(Blog blog){
+            _context.Blog.Add(blog);
+            await _context.SaveChangesAsync();
+        }
+        public async Task UpdateBlog(Blog blog){
+            var singleBlog = await _context.Blog.FindAsync(blog);
+            // have to ask for what I have to change
+        }
+        public async Task DeleteBlog(int id){
+            var BlogId = await _context.Blog            
+                                            .SingleOrDefaultAsync(blog => blog.id == id);
+            if(BlogId != null){
+                _context.Blog.Remove(BlogId);
+                //save changes 
+                await _context.SaveChangesAsync();
+            }
+
+        }
+        public async Task AddPost(Post post){
+            _context.Post.Add(post);
+            await _context.SaveChangesAsync();
+        }
+        public async Task UpdatePost(Post post){
+
+        }
+        public async Task DeletePost(int id){
+            var singlePost = await _context.Post    
+                                                .SingleOrDefaultAsync(post => post.id == id);
+            if(singlePost != null){
+                _context.Post.Remove(singlePost);
+                await _context.SaveChangesAsync();
+            }
+        }
+
 }
